9bbd48b5d267baa21f02d4509c83c67e67eed71c
Mike Callahan <michaelcallahan@Michaels-MacBook-Pro.local>
2017-01-13 09:29:39 -0800

    added StyleSheet

--- tClassComponentTemplate.js
+++ tClassComponentTemplate.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
 class putFileNameHere extends Component {
@@ -13,5 +13,14 @@ class putFileNameHere extends Component {
     }
 }
 
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
 //make this component available to the app
 export default putFileNameHere;
--- tFunctionalComponentTemplate.js
+++ tFunctionalComponentTemplate.js
@@ -1,6 +1,6 @@
 //import liraries
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
 const putFileNameHere = () => {
@@ -10,6 +10,16 @@ const putFileNameHere = () => {
         </View>
     );
 };
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
 
 //make this component available to the app
 export default putFileNameHere;
